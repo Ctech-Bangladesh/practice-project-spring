f65c6caf7c69123a7e35db9c4565cf4f9f99ac73
mumin-pervez <pervez.ist@gmail.com>
2023-12-09 10:23:51 +0600

    Role Base Access System project added

---
+++ role-base-access-system/src/test/java/com/ctech/rolebaseaccesssystem/RoleBaseAccessSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctech.rolebaseaccesssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBaseAccessSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
